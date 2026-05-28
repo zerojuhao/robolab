f052967af18d0140f06f15a296a027b8bd23678a
iamhardworking <2673193692@qq.com>
2026-05-28 12:07:49 +0800

    Update list envs filter for RPO tasks

--- scripts/tools/list_envs.py
+++ scripts/tools/list_envs.py
@@ -68,7 +68,7 @@ def main():
     index = 0
     # acquire all Isaac environments names
     for task_spec in gym.registry.values():
-        if "Atom" in task_spec.id:
+        if "RPO" in task_spec.id:
             # add details to table
             table.add_row([index + 1, task_spec.id, task_spec.entry_point, task_spec.kwargs["env_cfg_entry_point"]])
             # increment count
